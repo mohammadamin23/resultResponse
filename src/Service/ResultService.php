7fc11f308d477d5181dcf36313e74c6b59c417f7
mohammadamin23 <38126814+mohammadamin23@users.noreply.github.com>
2021-11-28 16:36:08 +0330

    change parameter additional type

--- src/Service/ResultService.php
+++ src/Service/ResultService.php
@@ -14,22 +14,23 @@ class ResultService
      *
      * @param $isDone boolean
      * @param $data
-     * @param array|null $additional
+     * @param $additional
      * @param $message string
      * @param int $status
      * @return \Illuminate\Http\JsonResponse|JsonResource|ResourceCollection
      */
-    public function response(bool $isDone , $data, array $additional = null , $message = '' , $status = 200)
+    public function response(bool $isDone , $data, $additional = null , $message = '' , $status = 200)
     {
         if ($data instanceof JsonResource || $data instanceof ResourceCollection) {
             return $data->additional(['isDone' => $isDone , 'message' => $message , $additional]);
         } else {
-            return response()->json(array(
+            $array = array(
                 'isDone' => $isDone,
                 'data' => $data,
                 $additional,
                 'message' => $message
-            ) , $status);
+            );
+            return response()->json(array_merge($array , $additional) , $status);
         }
     }
 }
