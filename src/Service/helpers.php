2ae4cfd9b69df279ad2fb614a92800927d25029f
mohammadamin23 <38126814+mohammadamin23@users.noreply.github.com>
2021-11-28 14:13:48 +0330

    fix helpers

--- src/Service/helpers.php
+++ src/Service/helpers.php
@@ -2,8 +2,8 @@
 
 use Prs\ResultResponse\Service\Result;
 
-if (! function_exists('result')) {
-    function result($isDone , $data , $message='' ,$status=200): array
+if (!function_exists('result')) {
+    function result($isDone , $data , $message='' ,$status=200)
     {
         return Result::response($isDone , $data , $message ,$status);
     }
